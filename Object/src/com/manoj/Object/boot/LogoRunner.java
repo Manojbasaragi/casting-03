58c6a5a174f19287acd2b0810abb4fc6a6724d3e
Manojbasaragi <basaragimanoj58@gmail.com>
2023-08-16 12:40:53 +0530

    this is toString commit

---
+++ Object/src/com/manoj/Object/boot/LogoRunner.java
@@ -0,0 +1,20 @@
+package com.manoj.Object.boot;
+
+import com.manoj.Object.app.Logo;
+
+public class LogoRunner {
+
+	public static void main(String[] args) {
+		System.out.println("invoking main in LogoRunner");
+		
+		Logo logo = new Logo("Techtron Solution", "Manoj", "Red", true, "Peocock");
+        System.out.println(logo);
+        
+        Logo logo1 = new Logo("GloboTech Industries", "Bharat", "Blue", true, "Lion");
+        System.out.println(logo1);
+        
+        Logo logo2 = new Logo("Dennies art", "Krishna", "Violet", false, "Cat");
+        System.out.println(logo2);
+	}
+
+}
